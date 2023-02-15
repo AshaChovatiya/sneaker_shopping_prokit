085b9d2f111a5fdd33e6cf151ef6b54875e88b8a
sojitradipalij <sojitradipali191@gmail.com>
2023-02-15 09:21:55 +0530

    [IMP] home screen and search screen

--- lib/fragment/SSSearchFragment.dart
+++ lib/fragment/SSSearchFragment.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:provider/provider.dart';
 import 'package:sneaker_shopping_prokit/model/SneakerShoppingModel.dart';
+import 'package:sneaker_shopping_prokit/providers/search_screen_provider.dart';
 import 'package:sneaker_shopping_prokit/screen/SSProductScreen.dart';
+import 'package:sneaker_shopping_prokit/utils/SSConstants.dart';
 import 'package:sneaker_shopping_prokit/utils/SSDataGenerator.dart';
 import 'package:sneaker_shopping_prokit/utils/SSWidgets.dart';
 
 class SSSearchFragment extends StatelessWidget {
   final List<SneakerShoppingModel> list = getSearchData();
+  final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,66 +25,147 @@ class SSSearchFragment extends StatelessWidget {
         ),
         title: Text("Search", style: boldTextStyle()),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            TextField(
-              controller: TextEditingController(),
-              obscureText: false,
-              textAlign: TextAlign.start,
-              maxLines: 1,
-              style: TextStyle(fontWeight: FontWeight.w400, fontStyle: FontStyle.normal, fontSize: 14, color: Color(0xff000000)),
-              decoration: sSInputDecoration(
-                context: context,
-                name: 'Search',
-                icon: Icon(Icons.search, color: Colors.grey.withOpacity(0.7), size: 24),
-              ),
-            ),
-            ListView.builder(
-              padding: EdgeInsets.only(top: 8, bottom: 8),
-              itemCount: list.length,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (_, index) {
-                return Padding(
-                  padding: EdgeInsets.only(top: 8, bottom: 8),
-                  child: InkWell(
-                    onTap: () {
-                      SSProductScreen(img: list[index].img!).launch(context);
-                    },
-                    child: Stack(
-                      alignment: Alignment.centerLeft,
-                      children: [
-                        Image(image: AssetImage(list[index].img!), height: 250, width: MediaQuery.of(context).size.width, fit: BoxFit.cover),
-                        Container(height: 250, color: Colors.black26),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                list[index].name!,
-                                style: boldTextStyle(color: Colors.white, size: 12),
-                              ),
-                              SizedBox(height: 8),
-                              Container(height: 1, width: 50, color: Colors.white),
-                              SizedBox(height: 8),
-                              Text(list[index].subtitle!, style: boldTextStyle(color: Colors.white))
-                            ],
-                          ),
-                        ),
-                      ],
+      body: ChangeNotifierProvider(
+        create: (_) => SearchScreenProvider(),
+        child: Consumer<SearchScreenProvider>(
+          builder: (context, _searchScreenProvider, _) {
+            return Padding(
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  TextField(
+                    controller: searchController,
+                    obscureText: false,
+                    textAlign: TextAlign.start,
+                    maxLines: 1,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Color(0xff000000)),
+                    decoration: sSInputDecoration(
+                      context: context,
+                      name: 'Search',
+                      icon: Icon(Icons.search,
+                          color: Colors.grey.withOpacity(0.7), size: 24),
                     ),
+                    onChanged: (val) {
+                      if (val.isNotEmpty) {
+                        _searchScreenProvider.onChangeSearch(true);
+                        _searchScreenProvider.searchProductCategories(
+                          search: searchController.text,
+                        );
+                      } else {
+                        _searchScreenProvider.onChangeSearch(false);
+                        _searchScreenProvider.searchProductCategories();
+                      }
+                    },
                   ),
-                );
-              },
-            )
-          ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  _searchScreenProvider.isSearchLoading
+                      ? Expanded(
+                          child: Center(child: CircularProgressIndicator()))
+                      : Expanded(
+                          child: _searchScreenProvider
+                                      .searchProductCategoriesList.length >
+                                  0
+                              ? ListView.builder(
+                                  padding: EdgeInsets.only(top: 0, bottom: 8),
+                                  itemCount: _searchScreenProvider
+                                      .searchProductCategoriesList.length,
+                                  // shrinkWrap: true,
+                                  // physics: NeverScrollableScrollPhysics(),
+                                  itemBuilder: (_, index) {
+                                    return Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 8, bottom: 8),
+                                      child: InkWell(
+                                        onTap: () {
+                                          SSProductScreen(img: list[index].img!)
+                                              .launch(context);
+                                        },
+                                        child: Stack(
+                                          alignment: Alignment.centerLeft,
+                                          children: [
+                                            Image.network(
+                                                _searchScreenProvider
+                                                            .searchProductCategoriesList[
+                                                                index]!
+                                                            .imageUrl ==
+                                                        ''
+                                                    ? imagePlaceHolder
+                                                    : imageBaseApi +
+                                                        _searchScreenProvider
+                                                            .searchProductCategoriesList[
+                                                                index]!
+                                                            .imageUrl
+                                                            .toString(),
+                                                height: 250,
+                                                width: MediaQuery.of(context)
+                                                    .size
+                                                    .width,
+                                                fit: BoxFit.cover),
+                                            Container(
+                                                height: 250,
+                                                color: Colors.black26),
+                                            Padding(
+                                              padding: EdgeInsets.only(left: 8),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    _searchScreenProvider
+                                                        .searchProductCategoriesList[
+                                                            index]!
+                                                        .name
+                                                        .toString(),
+                                                    style: boldTextStyle(
+                                                        color: Colors.white,
+                                                        size: 12),
+                                                  ),
+                                                  SizedBox(height: 8),
+                                                  Container(
+                                                      height: 1,
+                                                      width: 50,
+                                                      color: Colors.white),
+                                                  SizedBox(height: 8),
+                                                  Text(
+                                                      _searchScreenProvider
+                                                              .searchProductCategoriesList[
+                                                                  index]!
+                                                              .slug ??
+                                                          '',
+                                                      style: boldTextStyle(
+                                                          color: Colors.white))
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                )
+                              : Center(
+                                  child: Text(
+                                    'No Result Found',
+                                    style: boldTextStyle(
+                                        color: Colors.black, size: 12),
+                                  ),
+                                ),
+                        )
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:amplify_api/model_queries.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:sneaker_shopping_prokit/models/ModelProvider.dart';
+
+import '../schema/graph_mutation_query.dart';
 
 class ProductProvider extends ChangeNotifier {
   List<Product?> productList = [];
@@ -50,35 +54,49 @@ class ProductProvider extends ChangeNotifier {
     //     }))
     //     .response;
     // print("DData:- ${response.data}");
-    final request = ModelQueries.list(
-      Product.classType,
-    );
-    final response = await Amplify.API.query(request: request).response;
-    print("DData:- ${response.data?.items.length}");
-    if (response.data!.items.length > 0) {
+    // final request = ModelQueries.list(
+    //   Product.classType,
+    // );
+    var request = Amplify.API.query(
+        request: GraphQLRequest<String>(
+      document: OrderSchema.listProduct(),
+    ));
+    var response = await request.response;
+    if (jsonDecode(response.data!)['listProducts']['items'].length > 0) {
       productList.clear();
-      for (var i = 0; i < response.data!.items.length; i++) {
-        if (response.data!.items[i] != null) {
-          productList.add(response.data!.items[i]);
+      for (var i = 0;
+          i < jsonDecode(response.data!)['listProducts']['items'].length;
+          i++) {
+        if (jsonDecode(response.data!)['listProducts']['items'] != null) {
+          productList.add(Product.fromJson(
+              jsonDecode(response.data!)['listProducts']['items'][i]));
         }
       }
-      print("Data:------- ${productList.length}");
+      newArrivals.clear();
       for (var j = 0; j < productList.length; j++) {
         if (productList[j]!.isFeatured == true) {
           newArrivals.add(productList[j]);
         }
       }
     }
+    print("Data:------- ${productList.length}");
     getAllCategory();
     notifyListeners();
   }
 
   getProductItemData(String? productId) async {
+    var getProduct = OrderSchema.getSingleProduct(productId!);
     productDetailLoading = true;
+
     try {
-      final request = ModelQueries.get(Product.classType, '$productId');
-      final response = await Amplify.API.query(request: request).response;
-      productDataModel = response.data;
+      var operation = Amplify.API.query(
+          request: GraphQLRequest<String>(
+        document: getProduct,
+      ));
+      var response = await operation.response;
+      print(response.errors);
+      productDataModel =
+          Product.fromJson(jsonDecode(response.data!)['getProduct']);
       print("ProductItemData:------- ${productDataModel!.id}");
       productDetailLoading = false;
     } catch (e) {
@@ -94,7 +112,7 @@ class ProductProvider extends ChangeNotifier {
       final request = ModelQueries.get(ProductImage.classType, '$productId');
       final response = await Amplify.API.query(request: request).response;
       var data = response.data;
-      print("ProductItemData:------- ${data}");
+      print("ProductItemData:------- $data");
       productDetailLoading = false;
     } catch (e) {
       productDetailLoading = false;
@@ -104,26 +122,42 @@ class ProductProvider extends ChangeNotifier {
   }
 
   Future<void> getAllCategory() async {
-    final request = ModelQueries.list(
-      ProductCategory.classType,
-    );
-    final response = await Amplify.API.query(request: request).response;
-    if (response.data!.items.length > 0) {
-      print("getAllCategory:------- ${response.data!.items.length}");
-      // for (var j = 0; j < response.data!.items.length; j++) {
-      categoryList.addAll(response.data!.items);
-      // }
-      print("categoryList length:------- ${categoryList.length}");
-      print("categoryList length:------- ${categoryList.length}");
+    var getProduct = OrderSchema.getListProductCategories();
+    // final request = ModelQueries.list(
+    //   ProductCategory.classType,
+    // );
+    // final response = await Amplify.API.query(request: request).response;
+    var operation = Amplify.API.query(
+        request: GraphQLRequest<String>(
+      document: getProduct,
+    ));
+    var response = await operation.response;
+    if (jsonDecode(response.data!)['listProductCategories']['items'].length >
+        0) {
+      categoryList.clear();
+      for (var j = 0;
+          j <
+              jsonDecode(response.data!)['listProductCategories']['items']
+                  .length;
+          j++) {
+        categoryList.add(ProductCategory.fromJson(
+            jsonDecode(response.data!)['listProductCategories']['items'][j]));
 
-      for (var i = 0; i < productList.length; i++) {
-        for (var j = 0; j < response.data!.items.length; j++) {
-          if (productList[i]?.id == response.data!.items[j]!.id) {
-            print('---------------- true');
+
+        for (var i = 0; i < productList.length; i++) {
+          for (var j = 0;
+              j <
+                  jsonDecode(response.data!)['listProductCategories']['items']
+                      .length;
+              j++) {
+            if (productList[i]?.slug == jsonDecode(response.data!)['listProductCategories']['items'][j]['slug']) {
+              print('---------------- true');
+            }
           }
         }
       }
+      print("categoryList length:------- ${categoryList.length}");
+      notifyListeners();
     }
-    notifyListeners();
   }
 }
